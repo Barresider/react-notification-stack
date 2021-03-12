870641c090701182c71f56a1c38fa34db166a211
fabian.kleinhanss <fabian.kleinhanss@e-hoi.de>
2021-03-12 09:52:51 +0100

    removed dist folder

--- dist/NotificationStack.d.ts
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./NotificationStack.style.scss";
-import { NotificationStackProps, NotificationStackContextProps } from "./types";
-export declare const NotificationStackContext: React.Context<NotificationStackContextProps>;
-export declare const NotificationStack: React.FC<NotificationStackProps>;
-//# sourceMappingURL=NotificationStack.d.ts.map
--- dist/NotificationStackItem.d.ts
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import { NotificationStackItemProps } from "./types";
-export declare const NotificationStackItem: React.FC<NotificationStackItemProps>;
-//# sourceMappingURL=NotificationStackItem.d.ts.map
